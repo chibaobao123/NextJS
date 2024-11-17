12e7c4d5cefbe8e3b79ed2bc309219c0e2ac70e0
chibaobao123 <baobaochi631999@gmail.com>
2024-11-17 21:31:02 +0700

    useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,36 @@
 // import Link from "next/link";
 import Table from "@/Component/table";
 import { useEffect } from "react";
+import useSWR from "swr";
 
 // import x from "@/styles/app.module.css";
 // import y from "@/styles/test.module.css";
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  console.log(data);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch();
+  //     const data = await res.json();
+  //     console.log(data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="container">
+      <div>{data?.length}</div>
       {/* <ul>
         <li className={x["red"]}>
           <Link href="/facebook">
